7d5f5465fa6a8db7cf802baed985a6fb0caa3d32
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2024-03-23 12:40:00 +0700

    misc changes

--- src/nailit.nim
+++ src/nailit.nim
@@ -1,14 +1,12 @@
 # NailIt - a simple literate programming tool.
 
-# LIMITATION: Each code block surrounded by ``` MUST have a name.
-#             Will crash when regular nameless code blocks are used.
+# LIMITATIONS: @{code block references} must be on
+#              a single line
 
 import regex
 import std/[strutils, tables, strtabs, paths, os]
 import packages/docutils/[rst, rstgen, dochelpers]
 import docopt
-
-# import print
 
 type
   BlockType = enum
@@ -22,6 +20,11 @@ type
       name: string
     else:
       discard
+
+const
+  codeBlockPtn = re2"^```(\s*(.+))?"
+  codeBlockRefPtn = re2"(@\{(.+)\})"
+  codeBlockRefSpacesPtn = re2"(?m)^(\s*?)@\{(.+?)\}"
 
 ## Helper proc to process and insert blocks in the block list
 proc addBlock(
@@ -40,46 +43,50 @@ proc addBlock(
           # try to strip the any start and ending newlines (limited to 1 for now) 
           contentStripped = contentBuf
 
-        if contentStripped[0] == '\n':
-          contentStripped = contentStripped[1 ..^ 1]
-        if contentStripped[^1] == '\n':
-          contentStripped = contentStripped[0 ..^ 2]
+        if contentStripped.len == 1:
+          contentStripped = ""
+        else:
+          if contentStripped[0] == '\n':
+            contentStripped = contentStripped[1 ..^ 1]
+          if contentStripped[^1] == '\n':
+            contentStripped = contentStripped[0 ..^ 2]
 
         contentStripped
       ), # return value
     )
 
 ## Parse blocks from a file.
-## 
-## Code blocks begin with ``` <name>
-## Code blocks end with ```
-## 
+##
+## Code blocks may have names, but generally
+## they start and end with ```.
+##
+## To give a code block a name, you do ``` <name>
+##
 ## The other kind of block are Prose blocks,
 ## which is everything else in the file, and
 ## is treated as Nim's weird RST+MD combination.
 proc getBlocks(f: File): seq[Block] =
-  var totalBlocks: seq[Block] = @[]
-  var contentBuffer = ""
-  var nameBuffer = ""
-  var flushed = false
+  var
+    totalBlocks: seq[Block] = @[]
+    isCode = false
+
+  var
+    contentBuffer = ""
+    nextNameBuffer = ""
 
   for line in lines(f):
-    if (var m: RegexMatch2; line.match(re2"^```\s+(.+)", m)):
-      # the line after this must be a code block
-      totalBlocks.addBlock Prose, contentBuffer # commit the previous block
-      contentBuffer = ""
-      nameBuffer = line[m.group(0)]
-      flushed = true
-    elif line.strip() == "```": # the line after this must be a prose block
-      totalBlocks.addBlock Code, contentBuffer, nameBuffer # commit the previous block
-      contentBuffer = ".. raw:: html\n" # hack
-      flushed = true
-    else:
-      flushed = false
-      contentBuffer &= line & "\n"
+    if (var m: RegexMatch2; line.match(codeBlockPtn, m)):
+      totalBlocks.addBlock((if isCode: Code else: Prose), contentBuffer, nextNameBuffer)
 
-  if not flushed:
-    totalBlocks.addBlock Prose, contentBuffer
+      nextNameBuffer = (
+        if (m.group(1).a > -1) and (m.group(1).b > -1):
+          line[m.group(1)]
+        else: ""
+      )
+      contentBuffer = ""
+      isCode = not isCode
+    else:
+      contentBuffer &= line & "\n"
 
   return totalBlocks
 
@@ -102,12 +109,12 @@ proc weave(blocks: seq[Block]): string =
   for txblock in blocks:
     case txblock.kind
     of Code:
-      for m in txblock.content.findAll(re2"@\{(.+)\}"):
-        let keyName = txblock.content[m.captures[0]]
+      for m in txblock.content.findAll(codeBlockRefPtn):
+        let keyName = txblock.content[m.captures[1]]
         if reflist.hasKey keyName:
           reflist[keyName].inc txblock.name
         else:
-          debugEcho "WARNING: key " & keyName & " not found!"
+          stderr.writeLine "WARNING: key " & keyName & " not found!"
     of Prose:
       discard
 
@@ -127,7 +134,7 @@ proc weave(blocks: seq[Block]): string =
       let escapedCode = txblock.content
         .replace("<", "&lt;")
         .replace(">", "&gt;")
-        .replace(re2"(@\{(.+)\})", nameAsLink)
+        .replace(codeBlockRefPtn, nameAsLink)
       let normName = txblock.name.nimIdentBackticksNormalize()
 
       # start writing converted code block
@@ -164,53 +171,54 @@ proc weave(blocks: seq[Block]): string =
 ## Compile source code files from a literate program.
 proc tangle(blocks: seq[Block], dest: Path) =
   var codeBlkMap: Table[string, string]
-  codeBlkMap.clear()
 
   proc replaceReferencesWithContent(m: RegexMatch2, s: string): string =
     let keyName = s[m.group(1)]
+
     if codeBlkMap.hasKey keyName:
-      # indent each line with the same amout of spaces as
+      # indent each line with the same amount of spaces as
       # the indentation of the references
-      if (let initialSpaces = s[m.group(0)].replace("\n", ""); initialSpaces.len > 0):
-        var paddedCodeLines = "\n" & initialSpaces
-        var isInitialLine = true
+      let initialNLAndSpaces = s[m.group(0)]
+      if (
+        let initialSpaces = initialNLAndSpaces.replace("\n", "")
+        initialSpaces.len > 0
+      ):
+        var
+          paddedCodeLines = initialSpaces
+          isInitialLine = true
         for line in codeBlkMap[keyName].strip().splitLines():
           if isInitialLine:
             paddedCodeLines &= line & "\n"
             isInitialLine = false
           else:
-            paddedCodeLines &= initialSpaces & line & "\n"
-        #print paddedCodeLines
+            paddedCodeLines &= initialSpaces & line & '\n'
         return paddedCodeLines
-      #print codeBlkMap[keyName]
-      return s[m.group(0)] & codeBlkMap[keyName]
-    else:
-      debugEcho "WARNING: key " & keyName & " not found!"
-      return "; " & s # ; should be a comment line
+      return initialNLAndSpaces & codeBlkMap[keyName]
+
+    stderr.writeLine "WARNING: key " & keyName & " not found!"
+    return ""
 
   # fill codeblock mappings
   for txblock in blocks:
     case txblock.kind
     of Code:
       if codeBlkMap.hasKey txblock.name:
-        debugEcho "Warning: replacing code block " & txblock.name
+        stderr.writeLine "WARNING: replacing code block " & txblock.name
       codeBlkMap[txblock.name] = txblock.content
     of Prose:
       discard
 
   # modify the mappings with actual values
   for codeBlk in codeBlkMap.mvalues: # :(
-    var count = 0
-    for _ in codeBlk.findAll(re2"(\s+?)@\{(.+?)\}"):
-      count += 1
-    for _ in 0 .. count: # :(
-      codeBlk = codeBlk.replace(re2"(\s+?)@\{(.+?)\}", replaceReferencesWithContent)
+    for _ in 0 .. codeBlk.findAll(codeBlockRefSpacesPtn).len: # :(
+      codeBlk = codeBlk.replace(codeBlockRefSpacesPtn, replaceReferencesWithContent)
 
   for key in codeBlkMap.keys:
     if key[0] == '/':
       let outFileName = dest / Path(key[1 ..^ 1])
       outFileName.parentDir.string.createDir()
       outFileName.string.open(fmWrite).write(codeBlkMap[key])
+      stderr.writeLine "INFO: wrote to file " & outFileName.string
 
 proc intoHtmlTemplate(weaved: string, temp: string = "", title: string = ""): string =
   if temp.strip() == "":
@@ -243,24 +251,22 @@ proc intoHtmlTemplate(weaved: string, temp: string = "", title: string = ""): st
 proc displayBlocks(blocks: seq[Block]) =
   var num = 1
   for b in blocks:
-    echo "----------"
-    echo "Block ",
-      (
+    let blockTitle =
+      "Block " & (
         case b.kind
         of Prose: "P."
         of Code: "C."
-      ),
-      $num,
-      " -> ",
-      (
+      ) & $num & (
         case b.kind
         of Prose: ""
-        of Code: " \"" & b.name & "\" "
+        of Code: " \"" & b.name & "\""
       )
+    echo '-'.repeat(blockTitle.len)
+    echo blockTitle
+    echo '-'.repeat(blockTitle.len)
     num += 1
-    echo "::::::::::"
     echo b.content
-    echo "----------\n"
+    echo '-'.repeat(blockTitle.len) & '\n'
 
 when isMainModule:
   let args = """
@@ -281,7 +287,7 @@ when isMainModule:
 
   blocks = see what blocks NailIt sees.
   """.docopt(
-    version = "NailIt 0.1"
+    version = "NailIt 0.2"
   )
 
   let blocks = open($args["<source.md>"]).getBlocks()
